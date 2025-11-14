7249461ddcb139924756bc493814a6387f7d9667
bufferOverflow06 <sujanjacharya123@gmail.com>
2025-11-14 20:13:27 +0530

    example(SingleThread): Add basic Runnable example
    Introduce Example1.java, demonstrating fundamental multithreading
    concepts using the Runnable interface. It showcases creating and
    running multiple threads, including a concise lambda expression
    for thread definition.

---
+++ Single_Thread/Example1.java
@@ -0,0 +1,36 @@
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		Thread one = new Thread(new ThreadOne());
+		Thread two = new Thread(new ThreadTwo());
+		Thread three = new Thread(() -> {
+			for (int i = 0; i < 5; i++) {
+				System.out.println("Thread Three : " + i);
+			}
+		});
+
+		one.start();
+		two.start();
+		three.start();
+	}
+
+}
+
+class ThreadOne implements Runnable {
+	@Override
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("ThreadOne : " + i);
+		}
+	}
+}
+
+class ThreadTwo implements Runnable {
+	@Override
+	public void run() {
+		for (int i = 0; i < 15; i++) {
+			System.out.println("ThreadTwo : " + i);
+		}
+	}
+}
