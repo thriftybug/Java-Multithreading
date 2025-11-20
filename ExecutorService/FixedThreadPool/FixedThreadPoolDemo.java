c5248c9b3d744425a8932dd2eb789057d4de095f
bufferOverflow06 <sujanjacharya123@gmail.com>
2025-11-20 22:55:40 +0530

    feat(FixedThreadPool): Add fixed thread pool demo
    Introduce FixedThreadPoolDemo.java, demonstrating the use of
    `Executors.newFixedThreadPool()`. This example showcases how a fixed number of threads in the pool execute tasks concurrently, managing a queue of waiting tasks once all threads are busy.

---
+++ ExecutorService/FixedThreadPool/FixedThreadPoolDemo.java
@@ -0,0 +1,31 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class FixedThreadPoolDemo {
+
+	public static void main(String[] args) {
+		try (ExecutorService service = Executors.newFixedThreadPool(2)) {
+			for (int i = 0; i < 10; i++) {
+				service.execute(new Task(i));
+			}
+		}
+	}
+}
+
+class Task implements Runnable {
+	private final int taskId;
+
+	public Task(int taskId) {
+		this.taskId = taskId;
+	}
+
+	@Override
+	public void run() {
+		System.out.println("Task with id " + taskId + " being executed by Thread " + Thread.currentThread().getName());
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			throw new RuntimeException();
+		}
+	}
+}
